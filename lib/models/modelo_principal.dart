bc710838e81c05e8d6279bf945d371d5a84b3a3c
vitorSiquera <vitor.siqueira.simeao@gmail.com>
2026-04-08 19:46:28 -0300

    refatora modelo para Atividade com campos de treino

--- lib/models/modelo_principal.dart
+++ lib/models/modelo_principal.dart
@@ -1,13 +1,22 @@
-class Pedido {
-  final String descricao;
-  final int quantidade;
-  final double valor;
-  final String categoria;
+class Atividade {
+  final String nome;
+  final String tipo;
+  final int duracaoMinutos;
+  final int calorias;
+  final String intensidade;
+  final String data;
 
-  Pedido(this.descricao, this.quantidade, this.valor, this.categoria);
+  Atividade({
+    required this.nome,
+    required this.tipo,
+    required this.duracaoMinutos,
+    required this.calorias,
+    required this.intensidade,
+    required this.data,
+  });
 
   @override
   String toString() {
-    return 'Pedido{descricao: $descricao, quantidade: $quantidade, valor: $valor, categoria: $categoria}';
+    return 'Atividade{nome: $nome, tipo: $tipo, duracao: ${duracaoMinutos}min, calorias: $calorias kcal, intensidade: $intensidade, data: $data}';
   }
 }
